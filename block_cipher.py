cf12286861269c4de9c78920d5070a4e0ca0661e
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2024-02-24 09:12:57 -0600

    trabajo terminado

--- block_cipher.py
+++ block_cipher.py
@@ -1,111 +1,64 @@
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
 from PIL import Image
-import io
-import numpy as np
+from Crypto.Cipher import AES
+import os
 
-with Image.open('foto1.jpeg') as img:
-    byte_arr = io.BytesIO()
-    img.save(byte_arr, format='JPEG')
-    img_bytes = byte_arr.getvalue()
+# Genera una clave de 16 bytes
+key = os.urandom(16)
 
-# Definiendo la clave y el vector de inicialización (IV)
-clave = b'clavede16bytess_'
-iv = b'iv_de_16_bytes__'
+def pad(data):
+    return data + b"\x00" * (16 - len(data) % 16)
 
-# Creando el objeto de cifrado para cifrar
-cipher_ecb_encrypt = AES.new(clave, AES.MODE_ECB)
-cipher_cbc_encrypt = AES.new(clave, AES.MODE_CBC, iv)
+def encrypt_ecb(data, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.encrypt(pad(data))
 
-# Cifrando la imagen
-img_cifrada_ecb = cipher_ecb_encrypt.encrypt(pad(img_bytes, AES.block_size))
-img_cifrada_cbc = cipher_cbc_encrypt.encrypt(pad(img_bytes, AES.block_size))
+def encrypt_cbc(data, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    return cipher.encrypt(pad(data))
 
+# Ejemplo con formato jpeg
+with Image.open('foto1.jpeg') as im:
+    data = im.tobytes()
+
+# Haciendo los cifrados
+encrypted_ecb_data = encrypt_ecb(data, key)
+encrypted_cbc_data = encrypt_cbc(data, key)
+
+# Resultados
+with Image.frombytes('RGB', im.size, encrypted_ecb_data) as im_ecb:
+    im_ecb.save('imagen_ecb.jpg')
+
+with Image.frombytes('RGB', im.size, encrypted_cbc_data) as im_cbc:
+    im_cbc.save('imagen_cbc.jpg')
+
+# Ejemplo con formato webp
+with Image.open('Logo-UVG.webp') as im:
+    data1 = im.tobytes()
     
-# Creando un nuevo objeto de cifrado para descifrar
-cipher_ecb_decrypt = AES.new(clave, AES.MODE_ECB)
-cipher_cbc_decrypt = AES.new(clave, AES.MODE_CBC, iv)
+encrypted_ecb_data = encrypt_ecb(data1, key)
+encrypted_cbc_data = encrypt_cbc(data1, key)
 
-# Convirtiendo los bytes descifrados a una imagen
-img_descifrada_ecb = unpad(cipher_ecb_decrypt.decrypt(img_cifrada_ecb), AES.block_size)
-img_descifrada_cbc = unpad(cipher_cbc_decrypt.decrypt(img_cifrada_cbc), AES.block_size)
+# Resultados
+with Image.frombytes('RGB', im.size, encrypted_ecb_data) as im_ecb:
+    im_ecb.save('imagen_ecb.webp')
 
-# Y luego convertir los bytes descifrados de nuevo a una imagen
-img_ecb = Image.open(io.BytesIO(img_descifrada_ecb))
-img_cbc = Image.open(io.BytesIO(img_descifrada_cbc))
+with Image.frombytes('RGB', im.size, encrypted_cbc_data) as im_cbc:
+    im_cbc.save('imagen_cbc.webp')
 
-img_ecb.show()
-img_cbc.show()
+# Ejemplo con formato ppm
+with Image.open('tux.ppm') as im:
+    data1 = im.tobytes()
+    
+encrypted_ecb_data = encrypt_ecb(data1, key)
+encrypted_cbc_data = encrypt_cbc(data1, key)
 
-############################################################################################################
-with Image.open('Logo-UVG.webp') as img: 
-    byte_arr = io.BytesIO()
-    img.save(byte_arr, format='WEBP')  # Cambia 'JPEG' a 'WEBP'
-    img_bytes = byte_arr.getvalue()
+# Resultados
+with Image.frombytes('RGB', im.size, encrypted_ecb_data) as im_ecb:
+    im_ecb.save('imagen_ecb.ppm')
 
-# Definiendo la clave y el vector de inicialización (IV)
-clave = b'clavede16bytess_'
-iv = b'iv_de_16_bytes__'
+with Image.frombytes('RGB', im.size, encrypted_cbc_data) as im_cbc:
+    im_cbc.save('imagen_cbc.ppm')
 
-# Creando el objeto de cifrado para cifrar
-cipher_ecb_encrypt = AES.new(clave, AES.MODE_ECB)
-cipher_cbc_encrypt = AES.new(clave, AES.MODE_CBC, iv)
-
-# Cifrando la imagen
-img_cifrada_ecb = cipher_ecb_encrypt.encrypt(pad(img_bytes, AES.block_size))
-img_cifrada_cbc = cipher_cbc_encrypt.encrypt(pad(img_bytes, AES.block_size))
-
-# Creando un nuevo objeto de cifrado para descifrar
-cipher_ecb_decrypt = AES.new(clave, AES.MODE_ECB)
-cipher_cbc_decrypt = AES.new(clave, AES.MODE_CBC, iv)
-
-# Descifrando la imagen
-img_descifrada_ecb = unpad(cipher_ecb_decrypt.decrypt(img_cifrada_ecb), AES.block_size)
-img_descifrada_cbc = unpad(cipher_cbc_decrypt.decrypt(img_cifrada_cbc), AES.block_size)
-
-# Convirtiendo los bytes descifrados a una imagen
-img_ecb = Image.open(io.BytesIO(img_descifrada_ecb))
-img_cbc = Image.open(io.BytesIO(img_descifrada_cbc))
-
-
-img_ecb.save('img_descifrada_ecb.webp', 'WEBP')
-img_cbc.save('img_descifrada_cbc.webp', 'WEBP')  
-
-img_ecb.show()
-img_cbc.show()
-
-############################################################################################################
-
-with Image.open('tux.ppm') as img:
-    byte_arr = io.BytesIO()
-    img.save(byte_arr, format='PPM')  # Cambia 'JPEG' a 'PPM'
-    img_bytes = byte_arr.getvalue()
-
-# Definiendo la clave y el vector de inicialización (IV)
-clave = b'clavede16bytess_' 
-iv = b'iv_de_16_bytes__'
-
-# Creando el objeto de cifrado para cifrar
-cipher_ecb_encrypt = AES.new(clave, AES.MODE_ECB)
-cipher_cbc_encrypt = AES.new(clave, AES.MODE_CBC, iv)
-
-# Cifrando la imagen
-img_cifrada_ecb = cipher_ecb_encrypt.encrypt(pad(img_bytes, AES.block_size))
-img_cifrada_cbc = cipher_cbc_encrypt.encrypt(pad(img_bytes, AES.block_size))
-
-# Creando un nuevo objeto de cifrado para descifrar
-cipher_ecb_decrypt = AES.new(clave, AES.MODE_ECB)
-cipher_cbc_decrypt = AES.new(clave, AES.MODE_CBC, iv)
-
-# Descifrando la imagen
-img_descifrada_ecb = unpad(cipher_ecb_decrypt.decrypt(img_cifrada_ecb), AES.block_size)
-img_descifrada_cbc = unpad(cipher_cbc_decrypt.decrypt(img_cifrada_cbc), AES.block_size)
-
-img_ecb = Image.open(io.BytesIO(img_descifrada_ecb))
-img_cbc = Image.open(io.BytesIO(img_descifrada_cbc))
-
-img_ecb.save('img_descifrada_ecb.ppm', 'PPM')
-img_cbc.save('img_descifrada_cbc.ppm', 'PPM')
-
-img_ecb.show()
-img_cbc.show()
+# Enseñando las imagenes de los resultados
+Image.open('imagen_ecb.ppm').show()
+Image.open('imagen_cbc.ppm').show()
